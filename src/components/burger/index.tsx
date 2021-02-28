e2f0a018e15004353824c1e2aaeb0add09dca4a0
Victor Ryabkov <victor.ryabkov.business@gmail.com>
2021-02-28 16:48:06 +0300

    feature: add icons pack

--- src/components/burger/index.tsx
+++ src/components/burger/index.tsx
@@ -1,4 +1,7 @@
+import './stlye.less'
 import React from 'react'
+
+// import { ReactComponent as HomeIcon } from '@/assets/icons/home.svg'
 
 const BurgerButtonsMenu = (): JSX.Element => {
   return <div>Burger menu</div>
