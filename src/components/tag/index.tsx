f81f5509ff3cd10c5634b67f54652aadf4632cea
Victor Ryabkov <victor.ryabkov.business@gmail.com>
2021-03-15 00:36:33 +0300

    feature: tags line with active status

--- src/components/tag/index.tsx
+++ src/components/tag/index.tsx
@@ -3,12 +3,15 @@ import React, { FC } from 'react'
 
 type TagItemProps = {
   label: string
+  active?: boolean
 }
 
-const TagItem: FC<TagItemProps> = ({ label }): JSX.Element => {
+const TagItem: FC<TagItemProps> = ({ label, active }): JSX.Element => {
+  const formatedLabel = label.charAt(0).toUpperCase() + label.slice(1)
+
   return (
-    <div className="tag-container">
-      <div className="tag-component">{label}</div>
+    <div className={active ? 'tag-container active-tag' : 'tag-container'}>
+      <div className="tag-component">{formatedLabel}</div>
     </div>
   )
 }
--- src/templates/tagsLine/index.tsx
+++ src/templates/tagsLine/index.tsx
@@ -1,14 +1,42 @@
 import './style.less'
-import React from 'react'
+import React, { useRef } from 'react'
 
 // Components
 import TagItem from '@/components/tag'
 
+interface ITag {
+  label: string
+  active: boolean
+}
+
+const fakeTags: Array<ITag> = [
+  {
+    label: 'последнее',
+    active: true,
+  },
+  {
+    label: 'чарт',
+    active: false,
+  },
+  {
+    label: 'вдудь',
+    active: false,
+  },
+  {
+    label: 'рекомендации',
+    active: false,
+  },
+]
+
 const TagsLine = (): JSX.Element => {
+  const scrollLineRef = useRef(null)
+
   return (
     <div className="tagsLine-container">
-      <div className="tagsLine">
-        <TagItem label="Последнее" />
+      <div className="tagsLine" ref={scrollLineRef}>
+        {fakeTags.map((tag: ITag) => {
+          return <TagItem label={tag.label} active={tag.active} />
+        })}
       </div>
     </div>
   )
